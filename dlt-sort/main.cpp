4026468b24ec762c4d06a4e38ea951236fd6db45
Matthias Behr <mbehr@mcbehr.de>
2013-12-31 17:44:12 +0100

    Initial Commit

---
+++ dlt-sort/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  dlt-sort
+//
+//  Created by Matthias Behr on 31.12.13.
+//  Copyright (c) 2013 Matthias Behr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

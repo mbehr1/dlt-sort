7efcc6a39a58db4215ef8b70e7dd749d7de72598
Matthias Behr <mbehr@mcbehr.de>
2014-01-11 16:49:12 +0100

    added first set of test case with 4 tests.

--- dlt-sort-unittests/main.cpp
+++ dlt-sort-unittests/main.cpp
@@ -9,6 +9,13 @@
 #include "dlt-sort.h"
 #include "gtest/gtest.h"
 
+TEST(BASIC_ASSUMPTIONS, size_of_dlt_structs) {
+    ASSERT_EQ(sizeof(DltStorageHeader), 16) << "DltStorageHeader has wrong size. New dlt version or wrong compiler settings?";
+    ASSERT_EQ(sizeof(DltStandardHeader), 4);
+	ASSERT_EQ(sizeof(DltStandardHeaderExtra), 12);
+	ASSERT_EQ(sizeof(DltExtendedHeader), 10);
+}
+
 int main(int argc, char **argv){
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();

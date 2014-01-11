8f174e5bd678e0acdfc578b398b2c13f9910cb88
Matthias Behr <mbehr@mcbehr.de>
2014-01-11 15:47:26 +0100

    added unit test framework (googletest) and target for unit tests dlt-sort-unittests

---
+++ dlt-sort-unittests/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dlt-sort-unittests
+//
+//  Created by Matthias Behr on 11.01.14.
+//  Copyright (c) 2014 Matthias Behr. All rights reserved.
+//
+
+#include "dlt-sort.h"
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
